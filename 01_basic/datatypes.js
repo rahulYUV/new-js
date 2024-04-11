8f886babca6385d2427fac57927fac91fa45ef02
rahul yuv <138332085+rahulYUV@users.noreply.github.com>
2024-04-11 15:13:47 +0530

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,12 @@
+
+// alert("hello") we are using node js not browser
+//code must be readble
+ let name = "hitesh"
+ let age = 45;
+ let iloggedIN = true;
+ console.table([name,iloggedIN,age,]);
+ let state = null;
+ console.log(state);
+
+
+
