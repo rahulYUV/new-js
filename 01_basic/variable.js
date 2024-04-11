dbe4a98505abd55f3fd47284c6f54eeee2965c95
rahul yuv <138332085+rahulYUV@users.noreply.github.com>
2024-04-11 15:14:25 +0530

    first commit

---
+++ 01_basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 17734684
+let accountEMail = "rahul5687@gmail.com"
+var accountPassword = "12234"
+ accountCity = "jaipur"
+  
+    // accountID = 2 not allowed
+    console.log(accountId)
+    accountEMail = "fhas@gmail.com"
+    accountPassword = "248349"
+    accountCity = "Patna"
+   console.table([accountId,accountEMail,accountPassword,accountCity]);
+
+// prefer not to use var bcoz scope ';
