84e36b8fea64135f6b82f1e4e78dd628aba0f3c8
rahul yuv <138332085+rahulYUV@users.noreply.github.com>
2024-04-12 23:41:16 +0530

    first commit

---
+++ 01_basic/conversion.js
@@ -0,0 +1,16 @@
+/*let score = undefined
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let cityName = "patna"
+// " 33" = 33
+// "33abd" = nan */
+
+            let isLoggedIN = 1;
+            console.log(typeof isLoggedIN)
+            console.log(isLoggedIN);
+            let Booleanin = String(isLoggedIN)
+            console.log(typeof Booleanin)
+            console.log(Booleanin);
