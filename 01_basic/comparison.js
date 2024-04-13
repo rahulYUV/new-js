e8edf1f72e075388f5a1434126b8dbf8285222cd
rahul yuv <138332085+rahulYUV@users.noreply.github.com>
2024-04-13 20:43:14 +0530

    first commit

---
+++ 01_basic/comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 >1);
+// console.log(2 ==1);
+// console.log(2 <1);
+// console.log(2!=2);
+
+console.log("11" =="abcd");
+
+    console.log(null == 0);
+    console.log(null)
+    console.log(typeof null);
+   
+    console.log(2 > null)
+    console.log(2 > undefined)
+    // strict cheeck
+    console.log("22"===2)
